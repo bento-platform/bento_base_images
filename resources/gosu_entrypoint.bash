f423bd61977f849ab93503761ed44267481f8981
David Lougheed <david.lougheed@gmail.com>
2023-03-22 12:41:02 -0400

    feat: try moving set_gitconfig into base entrypoint by default

--- resources/gosu_entrypoint.bash
+++ resources/gosu_entrypoint.bash
@@ -2,5 +2,8 @@
 
 source /create_service_user.bash
 
+# Set up Git configuration if needed (useful for development images / containers)
+gosu bento_user /bin/bash -c '/set_gitconfig.bash'
+
 # Drop into bento_user from root and execute the CMD specified for the image
 exec gosu bento_user "$@"
