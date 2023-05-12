d1409330e83bbe8c805865d8a198ad31d5dfb591
David Lougheed <david.lougheed@gmail.com>
2023-05-12 13:52:18 -0400

    include python scripts in path

--- resources/create_service_user.bash
+++ resources/create_service_user.bash
@@ -8,3 +8,6 @@ echo "[/create_service_user.bash] using USER_ID=${USER_ID}"
 # Add the user
 useradd --shell /bin/bash -u "${USER_ID}" --non-unique -c "Bento container user" -m bento_user
 export HOME=/home/bento_user
+
+# Set up the path to include installed module Python scripts
+echo 'PATH=/home/bento_user/.local/bin/:$PATH' >> /home/bento_user/.bashrc
