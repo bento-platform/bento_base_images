e6d388b74f980527caf43b4c7887a105dd08e1d6
David Lougheed <david.lougheed@gmail.com>
2025-11-24 10:21:40 -0500

    properly specify --no-deps in /poetry_user_install_dev.bash

--- resources/poetry_user_install_dev.bash
+++ resources/poetry_user_install_dev.bash
@@ -2,8 +2,9 @@
 
 # Only available in Python base images
 # Do a `poetry install --with dev`-ish install with --user by exporting requirements
+# See https://github.com/python-poetry/poetry-plugin-export?tab=readme-ov-file#usage
 pip install poetry-plugin-export
 poetry export -f requirements.txt --with dev --output requirements.txt
-pip install --no-cache-dir --user -r requirements.txt
+pip install --no-cache-dir --no-deps --user -r requirements.txt
 rm requirements.txt
 pip install -e .
